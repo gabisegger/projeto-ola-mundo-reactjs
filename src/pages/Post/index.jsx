0ce1f129514d1524590118c4a5639d0f7237d6fa
gabisegger <gabi.segger@gmail.com>
2023-07-26 08:15:49 -0300

    Finalizando o desenvolvimento da página de post

--- src/pages/Post/index.jsx
+++ src/pages/Post/index.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import './Post.css';
+import { useParams } from 'react-router-dom';
+import posts from '../../json/posts.json';
+import PostModel from '../../components/PostModel';
+import ReactMarkdown from 'react-markdown';
 
 export default function Post() {
   const parameters = useParams();
 
-  console.log(parameters)
-  
+  const post = posts.find((post) => {
+    return post.id === Number(parameters.id);
+  })
+
+  console.log(post);
+
   return (
-    <h1> Post {parameters.id}</h1>
+    <PostModel 
+      coverPhoto={`/assets/posts/${post.id}/capa.png`}
+      title={post.titulo}
+    >
+      <div className="post-markdown-container">
+        <ReactMarkdown>
+          {post.texto}
+        </ReactMarkdown>
+      </div>
+      
+    </PostModel>
   )
 }
